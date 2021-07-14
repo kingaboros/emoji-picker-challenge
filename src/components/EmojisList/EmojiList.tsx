e0e2d06b661a03551222f7c582b79284f4951f75
Kinga Boros <kinga.boros20@gmail.com>
2021-07-14 14:50:40 +0300

    Added all the emoji categories in the list

--- src/components/EmojisList/EmojiList.tsx
+++ src/components/EmojisList/EmojiList.tsx
@@ -5,32 +5,229 @@ import Emojis from '../../emojisDb/emojisList.json';
 import * as classes from './EmojiList.module.scss';
 
 const emojiList = () => {
-  return;
-  <div className={classes.contentWrapper}>
-    {Emojis.map(emoji => {
-      console.log(emoji.category);
-      return (
-        <h4 className={classes.iconCategoryHeading} key={emoji.emoji}>
-          {emoji.category1}
-        </h4>
-      );
-    })}
+  return (
+    <div className={classes.contentWrapper}>
+      <div>
+        {Emojis.map((emoji: any) => {
+          return (
+            <h4
+              className={classes.iconCategoryHeading}
+              key={emoji.emojiSmileys}
+            >
+              {emoji.category1}
+            </h4>
+          );
+        })}
+      </div>
 
-    <div className={classes.emojiWrapper}>
-      <div className={classes.emojiRow}>
-        <div className={classes.emojis}>
-          {Emojis.map(emoji => {
-            console.log(emoji.category);
-            return (
-              <a className={classes.emojiIcons} key={emoji.description}>
-                {emoji.emojiSmileys}
-              </a>
-            );
-          })}
+      <div className={classes.emojiWrapper}>
+        <div className={classes.emojiRow}>
+          <div className={classes.emojis}>
+            {Emojis.map((emoji: any) => {
+              return (
+                <a className={classes.emojiIcons} key={emoji.emojiSmileys}>
+                  {emoji.emojiSmileys}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div>
+        {Emojis.map((emoji: any) => {
+          return (
+            <h4 className={classes.iconCategoryHeading} key={emoji.emojiPeople}>
+              {emoji.category2}
+            </h4>
+          );
+        })}
+      </div>
+
+      <div className={classes.emojiWrapper}>
+        <div className={classes.emojiRow}>
+          <div className={classes.emojis}>
+            {Emojis.map((emoji: any) => {
+              return (
+                <a className={classes.emojiIcons} key={emoji.emojiPeople}>
+                  {emoji.emojiPeople}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div>
+        {Emojis.map((emoji: any) => {
+          return (
+            <h4 className={classes.iconCategoryHeading} key={emoji.emojiNature}>
+              {emoji.category3}
+            </h4>
+          );
+        })}
+      </div>
+
+      <div className={classes.emojiWrapper}>
+        <div className={classes.emojiRow}>
+          <div className={classes.emojis}>
+            {Emojis.map((emoji: any) => {
+              return (
+                <a className={classes.emojiIcons} key={emoji.emojiNature}>
+                  {emoji.emojiNature}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div>
+        {Emojis.map((emoji: any) => {
+          return (
+            <h4 className={classes.iconCategoryHeading} key={emoji.emojiFood}>
+              {emoji.category4}
+            </h4>
+          );
+        })}
+      </div>
+
+      <div className={classes.emojiWrapper}>
+        <div className={classes.emojiRow}>
+          <div className={classes.emojis}>
+            {Emojis.map((emoji: any) => {
+              return (
+                <a className={classes.emojiIcons} key={emoji.emojiFood}>
+                  {emoji.emojiFood}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div>
+        {Emojis.map((emoji: any) => {
+          return (
+            <h4 className={classes.iconCategoryHeading} key={emoji.emojiTravel}>
+              {emoji.category5}
+            </h4>
+          );
+        })}
+      </div>
+
+      <div className={classes.emojiWrapper}>
+        <div className={classes.emojiRow}>
+          <div className={classes.emojis}>
+            {Emojis.map((emoji: any) => {
+              return (
+                <a className={classes.emojiIcons} key={emoji.emojiTravel}>
+                  {emoji.emojiTravel}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div>
+        {Emojis.map((emoji: any) => {
+          return (
+            <h4
+              className={classes.iconCategoryHeading}
+              key={emoji.emojiActivities}
+            >
+              {emoji.category6}
+            </h4>
+          );
+        })}
+      </div>
+
+      <div className={classes.emojiWrapper}>
+        <div className={classes.emojiRow}>
+          <div className={classes.emojis}>
+            {Emojis.map((emoji: any) => {
+              return (
+                <a className={classes.emojiIcons} key={emoji.emojiActivities}>
+                  {emoji.emojiActivities}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div>
+        {Emojis.map((emoji: any) => {
+          return (
+            <h4
+              className={classes.iconCategoryHeading}
+              key={emoji.emojiObjects}
+            >
+              {emoji.category7}
+            </h4>
+          );
+        })}
+      </div>
+
+      <div className={classes.emojiWrapper}>
+        <div className={classes.emojiRow}>
+          <div className={classes.emojis}>
+            {Emojis.map((emoji: any) => {
+              return (
+                <a className={classes.emojiIcons} key={emoji.emojiObjects}>
+                  {emoji.emojiObjects}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div>
+        {Emojis.map((emoji: any) => {
+          return (
+            <h4
+              className={classes.iconCategoryHeading}
+              key={emoji.emojiSymbols}
+            >
+              {emoji.category8}
+            </h4>
+          );
+        })}
+      </div>
+
+      <div className={classes.emojiWrapper}>
+        <div className={classes.emojiRow}>
+          <div className={classes.emojis}>
+            {Emojis.map((emoji: any) => {
+              return (
+                <a className={classes.emojiIcons} key={emoji.emojiSymbols}>
+                  {emoji.emojiSymbols}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div>
+        {Emojis.map((emoji: any) => {
+          return (
+            <h4 className={classes.iconCategoryHeading} key={emoji.emojiFlags}>
+              {emoji.category9}
+            </h4>
+          );
+        })}
+      </div>
+
+      <div className={classes.emojiWrapper}>
+        <div className={classes.emojiRow}>
+          <div className={classes.emojis}>
+            {Emojis.map((emoji: any) => {
+              return (
+                <a className={classes.emojiIcons} key={emoji.emojiFlags}>
+                  {emoji.emojiFlags}
+                </a>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default emojiList;
