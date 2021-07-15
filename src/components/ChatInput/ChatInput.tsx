d8b729f83568cb99d8c6e081f6d58ee9a647293d
Kinga Boros <kinga.boros20@gmail.com>
2021-07-15 10:44:40 +0300

    added the emoji icon on the input field

--- src/components/ChatInput/ChatInput.tsx
+++ src/components/ChatInput/ChatInput.tsx
@@ -1,36 +1,42 @@
 import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
+
+import { GoSmiley } from 'react-icons/go';
 
 import strings from '../../themes/strings';
 
 import * as classes from './ChatInput.module.scss';
 
 const ChatInput = (props: any) => {
-  const [showEmojis, setShowEmojis] = useState();
+  const [value, setValue] = useState('');
 
-  const handleShowEmojis = (e: any) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
-    setShowEmojis(showEmojis);
-    console.log('button clicked');
+    console.log(value);
+    setValue('');
   };
 
   return (
     <div className={classes.inputWrapper}>
       <div className={classes.inputArea}>
         <h1 className={classes.heading}>{strings.heading.main}</h1>
-        <input
-          type="text"
-          id="typeText"
-          className={classes.input}
-          placeholder={strings.input.inputField}
-        />
-        <Button
-          onClick={handleShowEmojis}
-          className={classes.emojiBtn}
-          type="button"
-        >
-          {strings.button.emojiBtn}
-        </Button>
+        <Form onClick={handleSubmit}>
+          <Form.Group className={classes.inputForm}>
+            <Form.Control
+              type="text"
+              value={value}
+              id="typeText"
+              className={classes.input}
+              placeholder={strings.input.inputField}
+              onChange={(e: any) => setValue(e.target.value)}
+            />
+
+            <GoSmiley className={classes.inputIcon} />
+          </Form.Group>
+          <Button className={classes.submitBtn} type="button">
+            {strings.button.submitBtn}
+          </Button>
+        </Form>
       </div>
     </div>
   );
